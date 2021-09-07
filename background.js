6d156067a8ed69184f79a8e8120652194ccd7b7f
happyer29 <happyer.present@gmail.com>
2021-09-07 19:57:04 +0300

    ready to tests

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 try {
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
-//     if(changeInfo.status === 'complete' && /^http/.test(tab.url)){
-//         chrome.scripting.executeScript({
-//             target: {tabId: tabId},
-//             files: ["./foreground.js"]
-//         })
-//             .then(() => {
-//                 console.log("injected")
-//         }).catch(err => console.log(err) )
-//     }
-// })
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
+    if(changeInfo.status === 'complete' && /^file/.test(tab.url)){ //change to http on prod
+        chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ["./foreground.js"]
+        })
+            .then(() => {
+                console.log("injected")
+        }).catch(err => console.log(err) )
+    }
+})
 
 
-console.log("test");
+//console.log("test");
 
 } catch (e) {
     console.error(e);
--- foreground.js
+++ foreground.js
@@ -1,19 +1,20 @@
-// function simulateClick(obj) {
-//     //const evt = document.createEvent("MouseEvents");
-//     //evt.initMouseEvent("click", true, true, window,
-//        // 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-//     //var canceled = !obj.dispatchEvent(evt);
-//     /*
-//
-//       if(canceled) {
-//         // A handler called preventDefault
-//         alert("canceled");
-//       } else {
-//         // None of the handlers called preventDefault
-//         alert("not canceled");
-//       }
-//       */
-// }
-//
-// var what = document.querySelector('.gb_f');
-// simulateClick(what);
+function simulateClick(obj) {
+    const evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("click", true, true, window,
+       0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    var canceled = !obj.dispatchEvent(evt);
+
+
+      if(canceled) {
+        // A handler called preventDefault
+        alert("canceled");
+      } else {
+        // None of the handlers called preventDefault
+        console.log(obj.className);
+      }
+
+}
+
+let what = document.querySelector('button[data-bn-type="button"][class^=" css-"]');
+setInterval(()=>simulateClick(what), 20);
+
