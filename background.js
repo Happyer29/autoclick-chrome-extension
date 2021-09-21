f16ede219eb2b6d55989db7561cd17db76e8f10a
HaPPyeR29 <happyer.present@gmail.com>
2021-09-21 19:46:08 +0300

    test jquery click

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
     if(changeInfo.status === 'complete' && /^http/.test(tab.url)){ //change to http on prod
         chrome.scripting.executeScript({
             target: {tabId: tabId},
-            files: ["./foreground.js"]
+            files: ["./libs/jquery-3.6.0.min.js", "./foreground.js"]
         })
             .then(() => {
                 console.log("injected")
--- foreground.js
+++ foreground.js
@@ -1,23 +1,26 @@
 //let what = document.querySelector('button[data-bn-type="button"][class^=" css-"]');
 let what = document.getElementsByClassName('css-5k5hy9');
 
-var interval = setInterval(()=>simulateClick(what), 20);
+let interval = setInterval(()=>simulateClick(what), 20);
 
 function simulateClick(obj) {
-    const evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, true, window,
-        0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    var canceled = !obj.dispatchEvent(evt);
-
-
-    if(canceled) {
-        // A handler called preventDefault
-        alert("canceled");
-    } else {
-        // None of the handlers called preventDefault
-        console.log(obj.className);
-        clearInterval(interval)
-    }
+    $(".lnXdpd").click((e) => {
+        console.log(e);
+    })
+    // const evt = document.createEvent("MouseEvents");
+    // evt.initMouseEvent("click", true, true, window,
+    //     0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    // let canceled = !obj.dispatchEvent(evt);
+    //
+    //
+    // if(canceled) {
+    //     // A handler called preventDefault
+    //     alert("canceled");
+    // } else {
+    //     // None of the handlers called preventDefault
+    //     console.log(obj.className);
+    //     clearInterval(interval)
+    // }
 
 }
 
