61d1564a140986f32c48a50ac7aa595f2df97fae
happyer29 <happyer.present@gmail.com>
2021-10-02 14:45:35 +0300

    now selecting max and buy buttons

--- foreground.js
+++ foreground.js
@@ -25,80 +25,52 @@ $(function(){
 //button.innerHTML = "Start Auto Clicker"
 //body.appendChild(button);
 
-$("#__APP").after("<button>Start Auto Clicker</button>");
-    var button = $("button").last()
-    var body = document.getElementsByTagName("body")[0];
-    button.on("click", function() {
-        console.log("test");
-    if(stop===0) {
-        stop = 1
-        console.log("stopped")
-        //this.innerHTML = "Start Auto Clicker"
-    } else if(stop===1) {
-        stop = 0
-        console.log("started")
-        //this.innerHTML = "Stop Auto Clicker"
-    }
-});
 
-// var clickEvent = new MouseEvent('click', {
-//     view: window,
-//     bubbles: true,
-//     cancelable: true
+
+
+
+
+
+
+// $("#__APP").after("<button>Start Auto Clicker</button>");
+//     var button = $("button").last()
+//     var body = document.getElementsByTagName("body")[0];
+//     button.on("click", function() {
+//         console.log("test");
+//     if(stop===0) {
+//         stop = 1
+//         console.log("stopped")
+//         //this.innerHTML = "Start Auto Clicker"
+//     } else if(stop===1) {
+//         stop = 0
+//         console.log("started")
+//         //this.innerHTML = "Stop Auto Clicker"
+//     }
 // });
-// var cancelled = !bttn.dispatchEvent(clickEvent);
-// if (cancelled) {
-//     // A handler called preventDefault.
-//     alert("cancelled");
-// } else {
-//     // None of the handlers called preventDefault.
-//     alert("not cancelled");
-// }
 
-var bttn = document.getElementsByClassName('css-8a1dsu');
-//var bttn = $(".css-8a1dsu").first()
-var stop = 1;
+    let bttnMax = contains("div", "Макс");
+    //let bttnMax = document.querySelector('div:contains(\'Макс\')');
+    //console.log(bttnMax.textContent);
+    //let bttn = document.querySelector('.css-1bpso26:first-of-type');
+    let bttn = contains("button", "Купить");
+    var stop = 0;
 
 
-setInterval(function(){
-    if(stop===0) {
-        let what = document.querySelector('.css-8a1dsu:first-of-type');
-        var coordinates = what.getBoundingClientRect();
-        //simulateClick(what)
-        //const evt = new MouseEvent("click", {"screenX": 0, "screenY": 0, "clientX": 0, "clientY": 0, "ctrlKey": false, "shiftKey": false, "altKey": false, "metaKey": false, "button": 0, "relatedTarget": null});
-        //let canceled = what.dispatchEvent(evt);
-        simulate(what, "click")
-    }
-}, 1000);
-
-function simulateClick(obj) {
-    // $("body").click((e) => {
-    //     console.log(e);
-    // })
-    // $(".lnXdpd").click((e) => {
-    //     console.log(e);
-    // })
-    //const evt = document.createEvent("MouseEvents");
-    const evt = new MouseEvent("click", {"screenX": 0, "screenY": 0, "clientX": 0, "clientY": 0, "ctrlKey": false, "shiftKey": false, "altKey": false, "metaKey": false, "button": 0, "relatedTarget": null});
-    // evt.initMouseEvent("click", true, true, window,
-    //     0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    let canceled = !obj.dispatchEvent(evt);
+    setInterval(function(){
+        console.log("interval");
+        simulateEvent(bttnMax, "click")
+        simulateEvent(bttn, "click")
+    }, 1000);
 
 
-    if(canceled) {
-        // A handler called preventDefault
-        alert("canceled");
-    } else {
-        // None of the handlers called preventDefault
-        console.log(obj.className);
-        clearInterval(interval)
+
+    function contains(selector, text) {
+        return Array.from(document.querySelectorAll(selector))
+            .find(el => el.textContent === text);
     }
 
-}
 
-
-
-    function simulate(element, eventName)
+function simulateEvent(element, eventName)
     {
         var options = extend(defaultOptions, arguments[2] || {});
         var oEvent, eventType = null;
